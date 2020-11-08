73bd7ca376d96211d18081be8f04046b269fd143
raghusaripalli <rvs1604@gmail.com>
2020-11-08 16:15:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Simply-Schedule-Backend" 
---
+++ src/main/java/com/productivity/simplyschedule/SimplyScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.productivity.simplyschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplyScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplyScheduleApplication.class, args);
+	}
+
+}
